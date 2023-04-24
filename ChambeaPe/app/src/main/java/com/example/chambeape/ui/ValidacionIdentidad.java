4101ce95b73ab6856287cfcfe7dc15ae8f635f72
Aokifree98 <100434103+Aokifree98@users.noreply.github.com>
2023-04-24 00:11:23 -0500

    Agregando interfaces
    Añadiendo interfaces de los demas activities

---
+++ ChambeaPe/app/src/main/java/com/example/chambeape/ui/ValidacionIdentidad.java
@@ -0,0 +1,16 @@
+package com.example.chambeape.ui;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.chambeape.R;
+
+public class ValidacionIdentidad extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_validacion_identidad);
+    }
+}
