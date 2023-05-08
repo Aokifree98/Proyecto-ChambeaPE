8887cfe0db19faf6dc1bc8563fb5b3d9bb71879f
GroznyC <76092127@continental.edu.pe>
2023-05-08 01:18:54 -0500

    Pantalla 5 y 6
    Mostrar los más buscados de trabajos y trabajadores

---
+++ ChambeaPe/app/src/main/java/com/glcm/chambeape/ItemMostrarTopTrabajador.java
@@ -0,0 +1,14 @@
+package com.glcm.chambeape;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ItemMostrarTopTrabajador extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_mostrar_top_trabajador);
+    }
+}
